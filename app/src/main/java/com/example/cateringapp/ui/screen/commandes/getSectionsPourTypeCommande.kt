add0567007152ad7a841897009806191d6b4def9
salma <salmasedrati18@gmail.com>
2025-08-04 18:48:11 +0100

    Modif des sections

--- app/src/main/java/com/example/cateringapp/ui/screen/commandes/getSectionsPourTypeCommande.kt
+++ app/src/main/java/com/example/cateringapp/ui/screen/commandes/getSectionsPourTypeCommande.kt
@@ -7,7 +7,7 @@ import com.example.cateringapp.data.dto.SectionProduit
 fun getSectionsPourTypeCommande(typeCommande: String): List<SectionProduit> {
     val supplement = mutableListOf<ProduitCommande>()
 
-    return when (typeCommande) {
+    return when (typeCommande.uppercase()) {
         "BUFFET_DE_SOUTENANCE" -> listOf(
             SectionProduit("Côté sucré", listOf(
                 ProduitCommande("Mini Soirées", "Côté sucré", 0.0),
@@ -22,10 +22,48 @@ fun getSectionsPourTypeCommande(typeCommande: String): List<SectionProduit> {
                 ProduitCommande("Sushis", "Côté salé", 0.0)
             ).toMutableStateList()),
             SectionProduit("Boisson", listOf(
-                ProduitCommande("Jus trois parfums nature (Cocktail à l’orange, Jus de citron gingembre, Jus de fraise)", "Boisson", 0.0),
+                ProduitCommande("Jus trois parfums nature: Cocktail à l’orange, citron gingembre, fraise", "Boisson", 0.0),
                 ProduitCommande("Café", "Boisson", 0.0),
                 ProduitCommande("Thé", "Boisson", 0.0),
                 ProduitCommande("Eau minérale", "Boisson", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Supplément", supplement.toMutableStateList())
+        )
+        "FTOUR_RAMADAN" -> listOf(
+            SectionProduit("Soupes", listOf(
+                ProduitCommande("Harira traditionnelle", "Soupes", 0.0),
+                ProduitCommande("Soupe chinoise", "Soupes", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Accompagnements", listOf(
+                ProduitCommande("Dattes, œufs durs", "Accompagnements", 0.0),
+                ProduitCommande("Chebakia, sellou, Briouates aux amandes", "Accompagnements", 0.0),
+                ProduitCommande("Mini viennoiserie", "Accompagnements", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Entremets", listOf(
+                ProduitCommande("Sablé au saumon fumé et cream cheese au miel", "Entremets", 0.0),
+                ProduitCommande("Toast à la mousse de foie gras", "Entremets", 0.0),
+                ProduitCommande("Feuilletés croquants au gambas", "Entremets", 0.0),
+                ProduitCommande("Nems viande hachée- crevette", "Entremets", 0.0),
+                ProduitCommande("Briouates poulet épinard - printanière", "Entremets", 0.0),
+                ProduitCommande("Quiches océanes", "Entremets", 0.0),
+                ProduitCommande("Mliwyat nature, Mliwyat au khliaa et Mkhemrat farcies", "Entremets", 0.0),
+                ProduitCommande("Pain, jben, fromage", "Entremets", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Jus et boissons chaudes", listOf(
+                ProduitCommande("Jus 3 parfums : cocktail à l'orange, fraise, avocat", "Jus et boissons chaudes", 0.0),
+                ProduitCommande("Lait froid et chaud", "Jus et boissons chaudes", 0.0),
+                ProduitCommande("Café, thé et eau minérale", "Jus et boissons chaudes", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Tajines", listOf(
+                ProduitCommande("Tajine de foie", "Tajines", 0.0),
+                ProduitCommande("Tajine pil-pil", "Tajines", 0.0),
+                ProduitCommande("Tajine cervelle", "Tajines", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Tradition", listOf(
+                ProduitCommande("Rzira et beghrir", "Tradition", 0.0)
+            ).toMutableStateList()),
+            SectionProduit("Pause-thé", listOf(
+                ProduitCommande("Thé avec Festival de gâteaux marocains (ghraiba aux noix et cornes de gazelle)", "Pause-thé", 0.0)
             ).toMutableStateList()),
             SectionProduit("Supplément", supplement.toMutableStateList())
         )
@@ -57,11 +95,14 @@ fun getSectionsPourTypeCommande(typeCommande: String): List<SectionProduit> {
                 ProduitCommande("4 Gâteaux amandes : k3ab, Kehk, ghraiba aux noix, la lune à l’orange", "Festival de gâteaux beldi", 0.0)
             )
             val diner = mutableListOf(
+                ProduitCommande("Trio salades: quinoa, Boruta et Guacamole", "Diner", 0.0),
                 ProduitCommande("Poulet dermira", "Diner", 0.0),
+                ProduitCommande("Duo pastillas", "Diner", 0.0),
                 ProduitCommande("Pastilla Poisson royale", "Diner", 0.0),
                 ProduitCommande("Mechwi d'agneau avec garniture", "Diner", 0.0),
-                ProduitCommande("Chwa avec garniture", "Diner", 0.0),
-                ProduitCommande("Tajine de veau contemporain", "Diner", 0.0)
+                ProduitCommande("Chwa malaki avec garniture", "Diner", 0.0),
+                ProduitCommande("Tajine de veau contemporain", "Diner", 0.0),
+                ProduitCommande("Accompagnement :assortiment de salades marocaines (Zaalouk, Beqola épinard, potiror caramélisé, cervelle, foi à la chermola)", "Diner", 0.0)
             )
             val dessert = mutableListOf(
                 ProduitCommande("Corbeille de fruits", "Côté dessert", 0.0),
